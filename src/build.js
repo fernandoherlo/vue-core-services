8c084494f8885e194f418acd5c66253a85d61acf
Fernando Hernáez <fernando.herlo@gmail.com>
2020-05-13 18:22:18 +0200

    Bugs

---
+++ src/build.js
@@ -0,0 +1,18 @@
+/**
+ * @name VueJS VueCore Services (vue-core-services)
+ * @description Library for Vue.js 2.0
+ */
+ // Services
+import auth from '@/services/auth'
+import http from '@/services/http'
+import api from '@/services/api'
+import acl from '@/services/acl'
+import helper from '@/services/helper'
+
+export {
+  auth,
+  http,
+  api,
+  acl,
+  helper
+}
